041eb462a7c310a22ff0f78498e2d586fd906b6c
孙显灏 <sunxianhao@rd.zmengzhu.com>
2019-08-06 17:52:08 +0800

    【调整】 调整布局图片及文案

--- PolyvCloudClassDemo/PolyvCloudClassDemo/AppDelegate.m
+++ PolyvCloudClassDemo/PolyvCloudClassDemo/AppDelegate.m
@@ -14,15 +14,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     /// 以下的直播字符串参数在官网（https://console.ruizaiyun.com/#/develop/appId）上已配置好
     PLVLiveVideoConfig *liveConfig = [PLVLiveVideoConfig sharedInstance];
-    liveConfig.channelId = ;
-    liveConfig.appId = ;
-    liveConfig.userId = ;
-    liveConfig.appSecret = ;
+    liveConfig.channelId = @"358806";
+    liveConfig.appId = @"fen6h6y31s";
+    liveConfig.userId = @"b27622c27e";
+    liveConfig.appSecret = @"9b30e9d334f74d02b08a1a2093ecd982";
     
-    /// 直播后，在（https://console.ruizaiyun.com/#/channel/你的频道号/playback）中可把某段视频转存到回放列表，然后在官网（https://my.polyv.net/secure/video/）上找到回放的 vodId 字符串值
+    /// 直播后，在（https://console.ruizaiyun.com/#/channel/你的频道号/playback）中可把某段视频转存到回放列表，然后在官网（http://console.ruizaiyun.com/#/channel/358806/videoLibrary）上找到回放的 vodId 字符串值
     PLVVodConfig *vodConfig = [PLVVodConfig sharedInstance];
-    vodConfig.vodId = ;
-    /// 以下字符串 configString，key，iv 的值在官网（https://my.polyv.net/secure/setting/api）上已配置好
+    vodConfig.vodId = @"ferwrq0xfj";
     NSError *error = nil;
     NSString *configString = @"TD1YSmNwb9igqvbRFuaBtZbrGfnKDTXOXi3quGttQ1yQDj2jeqri2K7QdS5QOAIqXdMhYmsVl/iV0J7rH6UcQu2v4s95/sH2DGR79ksc7gP8MbibWxMWUEB7DjYthJVVBw00jFgEkIAWxCr45Kjcxw==";/// SDK加密串
     NSString *key = @"VXtlHmwfS2oYm0CZ";/// 加密密钥
